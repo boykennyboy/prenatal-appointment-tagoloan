d186fefafe6a2150c9d0daf66180d91129fa924c
boykennyboy <kentdaniel533@gmail.com>
2025-10-04 17:24:39 +0800

    fix route

--- frontend/src/protected_route/GuestOnlyRoute.jsx
+++ frontend/src/protected_route/GuestOnlyRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router';
-import { useAuthStore } from '../store/AuthStore';
+import { useAuthStore } from '../store/authStore.js';
 
 const GuestOnlyRoute = ({ children }) => {
   const { token } = useAuthStore();
