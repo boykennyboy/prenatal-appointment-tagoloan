b54de3ea138af5544b7c788bc03ff08a67e966b0
boykennyboy <kentdaniel533@gmail.com>
2025-10-04 17:41:57 +0800

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useAuthStore } from '../store/AuthStore';
+import { useAuthStore } from '../store/authStore.js';
 
 const api = axios.create({
   baseURL: 'http://localhost:8000',
